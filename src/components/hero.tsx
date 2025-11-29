6af0f6b2d3867c9b1d8079bd9fd2dd6d1bc98e27
daniboycam <danielbatista1908112@gmail.com>
2025-11-29 19:22:31 -0300

    feat: remove desconto do hero.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -35,16 +35,10 @@ export function Hero(){
                             Ofereçemos os melhores vestidos e pijamas para garantir seu estilo e conforto em qualquer ocasião. 
                         </p>
 
-                            <a href = "https://wa.me/5561999785395" target = "_blank" className = "bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit gap-2">
+                            <a href = "https://wa.me/5561999785395" target = "_blank" className = "bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit gap-2 mb-8">
                                 <WhatsappLogoIcon className = "h-5 w-5"/>
                                 Contato via Whatsapp
                             </a>
-
-                        <div className = "mt-8">
-                            <p className = "text-sm mb-4">
-                                <b className = "bg-[#e93790] text-white px-2 py-1 rounded-md">5%</b> de desconto na primeira compra.
-                            </p>
-                        </div>
                     </div>
 
                     <div className = "hidden md:block h-full relative">
